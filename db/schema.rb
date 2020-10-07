596b8e3e3d37c80d80a9583a2a423e92870e3f19
ricoch <ricardo@rootstrap.com>
2020-10-07 20:22:37 -0300

    Run schema migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_171441) do
+ActiveRecord::Schema.define(version: 2020_10_07_053318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
+
+  create_table "balances", force: :cascade do |t|
+    t.bigint "external_payment_source_id"
+    t.decimal "change"
+    t.decimal "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["external_payment_source_id"], name: "index_balances_on_external_payment_source_id"
+  end
 
   create_table "delayed_jobs", id: :serial, force: :cascade do |t|
     t.integer "priority", default: 0, null: false
@@ -56,6 +65,33 @@ ActiveRecord::Schema.define(version: 2020_07_16_171441) do
     t.index ["error_group_id"], name: "index_exception_hunter_errors_on_error_group_id"
   end
 
+  create_table "external_payment_sources", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_external_payment_sources_on_user_id"
+  end
+
+  create_table "friendship_requests", force: :cascade do |t|
+    t.bigint "requester_id"
+    t.bigint "receiver_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["receiver_id"], name: "index_friendship_requests_on_receiver_id"
+    t.index ["requester_id", "receiver_id"], name: "index_friendship_requests_on_requester_id_and_receiver_id", unique: true
+    t.index ["requester_id"], name: "index_friendship_requests_on_requester_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_a_id"
+    t.bigint "user_b_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_a_id", "user_b_id"], name: "index_friendships_on_user_a_id_and_user_b_id", unique: true
+    t.index ["user_a_id"], name: "index_friendships_on_user_a_id"
+    t.index ["user_b_id"], name: "index_friendships_on_user_b_id"
+  end
+
   create_table "settings", force: :cascade do |t|
     t.string "key", null: false
     t.string "value"
@@ -74,5 +110,7 @@ ActiveRecord::Schema.define(version: 2020_07_16_171441) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "balances", "external_payment_sources"
   add_foreign_key "exception_hunter_errors", "exception_hunter_error_groups", column: "error_group_id"
+  add_foreign_key "external_payment_sources", "users"
 end
